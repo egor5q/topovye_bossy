414087107427bcbd463529d3ba4dff9109894502
egor5q <31898567+egor5q@users.noreply.github.com>
2019-02-19 20:00:36 +0300

    Update skills_classes.py

--- skills_classes.py
+++ skills_classes.py
@@ -17,3 +17,68 @@ bot = telebot.TeleBot(token)
 client=MongoClient(os.environ['database'])
 db=client.
 users=db.users
+
+
+class Skill:
+    
+    def __init__(self):
+        self.effects={}  # Все типы, которые есть в скилле. Например: self.effects={'attack':{...}}
+    
+class Type:
+    
+    def __init__(self):
+        self.targets='all'  # enemy/ally/all
+        self.cost=1
+        self.targets='all'
+        self.dmg=0
+        self.unit=None
+        self.cd=1           # cooldown
+    
+    
+class Attack(Type): 
+    
+    def __init__(self, dmg=0, targets='all', cd=1):
+        super().__init__()
+        self.targets=targets
+        self.dmg=dmg
+        self.cd=cd
+        
+        
+class Summon(Type):
+    
+    def __init__(self, unit=units.Rock, cd=1):
+        super().__init__()
+        self.unit=unit
+        self.cd=cd
+        
+        
+class Heal(Type):
+    
+    def __init__(self, heal, targets='ally', cd=1):
+        super().__init__()
+        self.heal=heal
+        self.targets=targets
+        self.cd=cd
+        
+        
+class Regenshield(Type):
+    
+    def __init__(self, regen, targets='ally', cd=1):
+        super().__init__()
+        self.regen=regen
+        self.targets=targets
+        self.cd=cd
+        
+        
+class Debuff(Type):
+    
+    def __init__(self, effect, targets='enemy', cd=1):
+        super().__init__()
+        self.effect=effect
+        self.targets=targets
+        self.cd=cd
+        
+        
+
+
+    
