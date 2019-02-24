a5826ff1de8533575b00f8c02d9b23f22d73d018
egor5q <31898567+egor5q@users.noreply.github.com>
2019-02-24 14:12:30 +0300

    Update unit_classes.py

--- unit_classes.py
+++ unit_classes.py
@@ -22,21 +22,7 @@ users=db.users
 class Unit:
     
     def __init__(self):
-        self.class=None  #tank/healer/damager/universal/summoner/buffer
-        self.maxhp=1000
-        self.maxattack=1000
-        self.maxshield=1000
-        self.skillmaxdmg=1000
-        self.skillmaxdmg=1000
-        self.maxsummons=0
-        self.activebuffs=0
-        self.activedebuffs=0
-        self.passives=1
-        self.attackskills=1
-        self.actives=1
-        self.dmgskills=1
-        self.passivelist=[]
-        self.activelist=[]
+        self.class=None  #tank/robot/human/turret
 
 
     
@@ -45,80 +31,30 @@ class Tank(Unit):
     def __init__(self):
         super().__init__()
         self.class='tank'
-        self.maxhp=9000
-        self.maxattack=1000
-        self.maxshield=10000
-        self.passives=3
-        self.actives=6
-        self.skillmaxdmg=2500
+
         
         
-class Universal(Unit):
+class Robot(Unit):
     
     def __init__(self):
         super().__init__()
-        self.class='universal'
-        self.maxhp=8500
-        self.maxattack=3000
-        self.maxshield=8500
-        self.passives=2
-        self.actives=7
-        self.skillmaxdmg=5000
+        self.class='robot'
+
         
         
-class Summoner(Unit):
+class Human(Unit):
     
     def __init__(self):
         super().__init__()
-        self.class='summoner'
-        self.maxhp=7000
-        self.maxattack=2000
-        self.maxshield=5000
-        self.passives=3
-        self.actives=8
-        self.skillmaxdmg=0
-        self.maxsummons=3
+        self.class='human'
+
         
         
-class Buffer(Unit):
+class Turret(Unit):
     
     def __init__(self):
         super().__init__()  
-        self.class='buffer'
-        self.maxhp=7000
-        self.maxattack=1000
-        self.maxshield=8000
-        self.passives=2
-        self.actives=7
-        self.activebuffs=4
-        self.activedebuffs=3
-        
-        
-class Attacker(Unit):
-    
-    def __init__(self):
-        super().__init__()
-        self.class='attacker'
-        self.maxhp=6000
-        self.maxattack=6000
-        self.maxshield=6500
-        self.passives=2
-        self.actives=6
-        self.skillmaxdmg=7000
-        
-        
-    
-class Healer(Unit):
-    
-    def __init__(self):
-        super().__init__()
-        self.class='healer'
-        self.maxhp=7000
-        self.maxattack=2000
-        self.maxshield=4500
-        self.passives=3
-        self.actives=6
-        self.attackskills=1
-        self.skillmaxdmg=2500
+        self.class='turret'
+
         
 
