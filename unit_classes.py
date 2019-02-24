ed5988d0dec2c92c30fbca48b5edb2278e6d596a
egor5q <31898567+egor5q@users.noreply.github.com>
2019-02-24 22:00:48 +0300

    Update unit_classes.py

--- unit_classes.py
+++ unit_classes.py
@@ -7,7 +7,6 @@ import threading
 from emoji import emojize
 from telebot import types
 from pymongo import MongoClient
-import unit_classes
 
 
 token = os.environ['TELEGRAM_TOKEN']
@@ -45,7 +44,8 @@ class Robot(Unit):
         super().__init__()
         self.class='robot'
 
-        
+   
+
         
 class Human(Unit):
     
@@ -60,6 +60,41 @@ class Turret(Unit):
     def __init__(self):
         super().__init__()  
         self.class='turret'
+        
+        
+class Test(Tank):
+    
+    def __init__(self):
+        super().__init__()
+        self.data='test'
+        self.name='танк'
+        self.photo='AgADAgADIqsxG_S_mEtSXlaP3lYsFAlsUw8ABCshaU23n-1NiYwAAgI'
+        
+class Test1(Robot):
+    
+    def __init__(self):
+        super().__init__()
+        self.data='робот'
+        self.name='имя1'
+        self.photo='AgADAgADfqoxG8C9mUu53zMx4nEzjF52Uw8ABIgfsfgEkuMyWY4AAgI'
+        
+        
+class Test2(Turret):
+    
+    def __init__(self):
+        super().__init__()
+        self.data='test2'
+        self.name='турель'
+        self.photo='AgADAgADKasxG_S_mEvGKrGBKHqqj6t3Uw8ABEXfZPnwvpCIJo4AAgI'
+        
+        
+class Test3(Weapon):
+    
+    def __init__(self):
+        super().__init__()
+        self.data='test3'
+        self.name='оружие'
+        self.photo='AgADAgADKqsxG_S_mEurpfu42MBjZRSXOQ8ABCeg1E-5kdAMZ_MEAAEC'
 
         
 
