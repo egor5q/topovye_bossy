e756cdb42b6a8919ece5871c080f10881ac06004
egor5q <31898567+egor5q@users.noreply.github.com>
2019-02-24 14:16:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,31 +18,6 @@ client=MongoClient(os.environ['database'])
 db=client.
 users=db.users
 
-@bot.message_handler(commands=['createboss'])
-def createboss(m):
-    x=users.find({})
-    reg=False
-    for ids in x:
-        if ids['id']==m.from_user.id:
-            reg=True
-    if reg==False:
-        users.insert_one(createuser(m.from_user))
-    user=users.find_one({'id':m.from_user.id})
-    if user['createcount']>0:
-        try:
-            x=m.text.split(' ')[1]
-        except:
-            x=None
-        if x!=None:
-            if x.lower()=='танк':
-                unit=unit_classes.Tank()
-            elif x.lower()=='хилер':
-                unit=unit_classes.Healer()
-            elif x.lower()=='дамагер':
-                unit=unit_classes.Damager()
-            elif x.lower()=='универсал':
-                unit=unit_classes.Universal()
-            bot.send_message(m.chat.id, 'Теперь отправьте характеристики вашего босса.')
         
         
 
@@ -50,9 +25,7 @@ def createuser(user):
     return {
         'id':user.id,
         'name':user.first_name,
-        'username':user.username,
-        'createdbosses':{},
-        'createcount':8
+        'username':user.username
     }
         
         
