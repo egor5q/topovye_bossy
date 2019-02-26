2975eb05ac6cdc9bec3fde503547dc52f1b7445f
egor5q <31898567+egor5q@users.noreply.github.com>
2019-02-26 23:14:45 +0300

    Create tools.py

---
+++ tools.py
@@ -0,0 +1,8 @@
+import os
+import telebot
+token = os.environ['TELEGRAM_TOKEN']
+bot = telebot.TeleBot(token)
+
+def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode=None):
+    return bot.edit_message_text(chat_id=chat_id,message_id=message_id,text=message_text,reply_markup=reply_markup,
+                                 parse_mode=parse_mode)  
